a61e91217402d5072b75a4189d045ae5d104154a
RicardoRios <72361340+RicardoRios07@users.noreply.github.com>
2026-02-04 14:03:18 -0500

    Fix: Update MongoDB connection URI to use environment variable

--- src/Config/Database/configMongo.js
+++ src/Config/Database/configMongo.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // Conexión a Base de datos
-const uri = `mongodb://localhost:27017/barrios`;
-// console.log('PASS',process.env.PASSWORD);
-// console.log('USEERNAME MONGO',process.env.USER_MONGO);
-
+const uri = process.env.MONGODB_URI || `mongodb://localhost:27017/barrios`;
 
 mongoose
     .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
